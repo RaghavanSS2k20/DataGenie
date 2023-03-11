ad4dad13dc7c5547f377dd783b5067bde52d0b56
Raghavan M <20pw26@psgtech.ac.in>
2023-03-12 00:28:35 +0530

    Revert to 364441e6ff1eb1165ceece57e40b36f6379d9485

---
+++ ETS.py
@@ -0,0 +1,11 @@
+from statsmodels.tsa.exponential_smoothing.ets import ETSModel
+def forcast_ETS(df):
+    try:
+        model = ETSModel(df,error='add', trend='add', seasonal='add', seasonal_periods=365)
+        fit = model.fit()
+        forecast = fit.forecast(steps=365)
+        return forecast
+    
+    except ValueError:
+        return "THIS MODEL CANNOT BE USED FOR THIS DATA"
+
